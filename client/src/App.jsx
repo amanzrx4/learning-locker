fdfc598875c1c64a5eb1b7b0af89589050241adb
pranish <pranishshrestha181@gmail.com>
2022-11-11 23:55:17 +0545

    feat: navbar added with react-router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,20 +2,41 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Button } from "@mui/material";
-import Login from "./components/Login";
+// import Login from "./components/Login";
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Link,
+  Routes,
+  BrowserRouter,
 } from "react-router-dom";
+import Home from "./pages/Home";
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
       <h1>Learning locker</h1>
-      <Login />
+      {/* <Login /> */}
+      <BrowserRouter>
+        <Routes>
+          
+          {" "}
+           
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<About />} /> */}
+       
+          <Route
+            path="*"
+            element={
+              <main style={{ padding: "1rem" }}>
+                <p>There's nothing here!</p>
+              </main>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Login from '../components/Login'
+import NavLink from '../components/Navbar/NavLink'
+
+function Home() {
+  return (
+    <div>
+        <Login />
+        <NavLink />
+    </div>
+  )
+}
+
+export default Home
