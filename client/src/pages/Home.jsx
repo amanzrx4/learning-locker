3360fda4948d059bc04163014546d9c3e718e614
pranish <pranishshrestha181@gmail.com>
2022-11-12 00:14:35 +0545

    fix: navbar boostrap added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Login from '../components/Login'
 import NavLink from '../components/Navbar/NavLink'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function Home() {
   return (
     <div>
